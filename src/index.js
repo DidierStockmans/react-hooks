fce404fee3e6565190b7eb95a457ee37f1e76600
Didier Stockmans <stockmans.didier@gmail.com>
2019-02-15 21:00:43 +0100

    Removed components, service worker, manifest. Modified index.html. Created hooks example.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const BreweryList = () => {
+  const [breweries, setBreweries] = useState([]);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  useEffect(() => {
+    fetch("https://api.openbrewerydb.org/breweries")
+      .then(response => response.json())
+      .then(data => {
+        setBreweries(data); // set breweries in state
+      });
+  }, []);
+
+  return (
+    <div className="section">
+      {breweries.map(({ id, name, website_url, city, state }) => (
+        <div key={id} className="card">
+            <h3><a href={website_url} target="_blank">{name}</a></h3>
+          <h3>
+            {city}, {state}
+          </h3>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<BreweryList />, rootElement);
